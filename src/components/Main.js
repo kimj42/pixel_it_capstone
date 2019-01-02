6ca913638f732136755337f1193fb97b1fe914b7
Karis Kim <ouniiq@hotmail.com>
2019-01-02 11:48:03 -0800

    created react app and created header

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+      <canvas id="canvas"></canvas>
+      </div>
+    );
+  }
+};
+
+
+export default Main;
